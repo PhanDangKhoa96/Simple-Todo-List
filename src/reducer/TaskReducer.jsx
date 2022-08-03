41c33c55ccb811570603a5a8f6e5732bdc283b79
Kurapika <kurapikachu123456@gmail.com>
2022-08-03 08:22:47 +0700

    Change name and favicon of project

--- src/reducer/TaskReducer.jsx
+++ src/reducer/TaskReducer.jsx
@@ -20,7 +20,6 @@ const taskReducer = (state, action) => {
         case TaskAction.UPDATE_TASK: {
             const task = state.find(task => task.id === payload)
             state = state.filter(task => task.id !== payload)
-            task.isDone = !task.isDone
 
             localStorage.setItem('tasks', JSON.stringify([...state, { ...task, isDone: !task.isDone }]))
 
